ae9c736d51d6023a046a7cbfdb7a33dc21a6ae77
Willamy Ungary <willamy.ungary@gmail.com>
2026-04-13 15:14:42 -0300

    criada aplicação media

---
+++ media.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+
+    // Declarando de variáveis
+    float nota1, nota2, nota3, nota4, media;
+
+    // Lendo as notas inseridas pelo usuário
+    printf("Digite a primeira nota: ");
+    scanf("%f", &nota1);
+    printf("Digite a segunda nota: ");
+    scanf("%f", &nota2);
+    printf("Digite a terceira nota: ");
+    scanf("%f", &nota3);
+    printf("Digite a quarta nota: ");
+    scanf("%f", &nota4);
+
+    // Calculando a média das notas
+    media = (nota1 + nota2 + nota3 + nota4) / 4;
+
+    // Exibindo o resultado da média
+    printf("A média das notas é: %f\n", media);
+
+    return 0;
+}
