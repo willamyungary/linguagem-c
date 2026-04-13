880e90d429fd16f1c9c3941a843674a98ad8b6e6
Willamy Ungary <willamy.ungary@gmail.com>
2026-04-13 17:32:57 -0300

    atualização
    foi realizada através da estrutura condicionado if, a verificação se o aluno está aprovado, em recuperação ou reprovado.

--- media.c
+++ media.c
@@ -3,23 +3,32 @@
 int main() {
 
     // Declaração de variáveis
-    float nota1, nota2, nota3, nota4, media;
+        float nota1, nota2, nota3, nota4, media;
 
     // Lendo as notas inseridas pelo usuário
-    printf("Digite a primeira nota: ");
-    scanf("%f", &nota1);
-    printf("Digite a segunda nota: ");
-    scanf("%f", &nota2);
-    printf("Digite a terceira nota: ");
-    scanf("%f", &nota3);
-    printf("Digite a quarta nota: ");
-    scanf("%f", &nota4);
+        printf("Digite a primeira nota: ");
+        scanf("%f", &nota1);
+        printf("Digite a segunda nota: ");
+        scanf("%f", &nota2);
+        printf("Digite a terceira nota: ");
+        scanf("%f", &nota3);
+        printf("Digite a quarta nota: ");
+        scanf("%f", &nota4);
 
     // Calculando a média das notas
     media = (nota1 + nota2 + nota3 + nota4) / 4;
 
     // Exibindo o resultado da média
-    printf("A média das notas é: %.2f\n", media);
+        printf("A média das notas é: %.2f\n", media);
+
+    // Verificando a situação do aluno com base na média
+        if (media >= 7.0) {
+            printf("Aprovado!\n");
+        } else if (media >= 5.0) {
+            printf("Recuperação!\n");
+        } else {
+            printf("Reprovado!\n");
+        }
 
     return 0;
 }
